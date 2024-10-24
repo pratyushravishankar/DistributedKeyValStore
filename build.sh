cc0df741e41fdf8ab7491de4d6464c4f9685be9d
pratyushravishankar <pratyushravishankar@gmail.com>
2024-10-24 15:15:52 +0100

    Initial commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/clang++ -std=c++20 -stdlib=libc++ -fcolor-diagnostics -fansi-escape-codes -g /Users/pratyush/DistributedKeyValueStore/main.cpp -o /Users/pratyush/DistributedKeyValueStore/main
