5c66e2a10b225d5cd52c5f3e63826cd1eca2fa87
pratyushravishankar <pratyushravishankar@gmail.com>
2024-11-13 12:32:18 +0000

    removed external/grpc folder, and modified CMake files to build exclusively with package-managed files

--- server/src/PartitionedHashMap.cpp
+++ server/src/PartitionedHashMap.cpp
@@ -1,4 +1,5 @@
 #include "PartitionedHashMap.h"
+#include <iostream>
 
 PartitionedHashMap::PartitionedHashMap(std::string_view filename, size_t numPartitions)
     : numPartitions(numPartitions)
@@ -18,17 +19,20 @@ size_t PartitionedHashMap::getPartitionIndex(const std::string &key)
 void PartitionedHashMap::insert(const std::string &key, const std::string &value)
 {
     size_t index = getPartitionIndex(key);
+    std::cout << "inserting onto partition: " << index << std::endl;
     partitions[index].insert(key, value);
 }
 
 std::optional<std::string> PartitionedHashMap::get(const std::string &key)
 {
     size_t index = getPartitionIndex(key);
+    std::cout << "getting from partition:" << index << std::endl;
     return partitions[index].get(key);
 }
 
 bool PartitionedHashMap::erase(const std::string &key)
 {
     size_t index = getPartitionIndex(key);
+    std::cout << "deleting from partition: " << index << std::endl;
     return partitions[index].erase(key);
 }
