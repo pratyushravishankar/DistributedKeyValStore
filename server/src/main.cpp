7ef4549e3978503b69b861af74b12368839ee10b
pratyushravishankar <pratyushravishankar@gmail.com>
2025-02-06 16:47:07 +0000

    fixed build issues

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -10,37 +10,45 @@ using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 
-struct ServerWithService {
+struct ServerWithService
+{
     std::unique_ptr<grpc::Server> server;
     std::unique_ptr<HashMapServiceImpl> service;
 };
 
-ServerWithService createServer(const ServerInfo& serverInfo, size_t virtualInstances, const ServerConfig& config) {
+ServerWithService createServer(const ServerInfo &serverInfo, size_t virtualInstances, const ServerConfig &config)
+{
     auto service = std::make_unique<HashMapServiceImpl>(serverInfo, virtualInstances, config);
     grpc::ServerBuilder builder;
     builder.AddListeningPort(serverInfo.address, grpc::InsecureServerCredentials());
     builder.RegisterService(service.get());
     auto server = builder.BuildAndStart();
-    return ServerWithService(std::move(server), std::move(service));
+    return {
+        .server = std::move(server),
+        .service = std::move(service)};
 }
 
-void startServers() {
-    const auto& config = ServerConfigManager::getInstance();
+void startServers()
+{
+    const auto &config = ServerConfigManager::getInstance();
     std::vector<std::thread> serverThreads;
 
-    for (const auto& serverInfo : config.servers) {
-        serverThreads.emplace_back([&]() {
+    for (const auto &serverInfo : config.servers)
+    {
+        serverThreads.emplace_back([&]()
+                                   {
             auto server = createServer(serverInfo, 1, config);
-            server.server->Wait();
-        });
+            server.server->Wait(); });
     }
 
-    for (auto& thread : serverThreads) {
+    for (auto &thread : serverThreads)
+    {
         thread.join();
     }
 }
 
-int main() {
+int main()
+{
     startServers();
     return 0;
 }
